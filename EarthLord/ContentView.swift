a3fe92a49472107e3826e14abe8f94abcc03676c
Wayne Fan <waynefan1756@gmail.com>
2026-01-01 20:59:18 +0100

    Initial Commit

---
+++ EarthLord/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EarthLord
+//
+//  Created by 范有为 on 2026/1/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EarthLord/EarthLordApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthLordApp.swift
+//  EarthLord
+//
+//  Created by 范有为 on 2026/1/1.
+//
+
+import SwiftUI
+
+@main
+struct EarthLordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
